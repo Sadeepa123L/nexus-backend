de09077362e4dfede49c39dcb5ca21a27f2ed314
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-08-09 19:36:53 +0530

    build: Add Eureka Client and Validation dependencies to workspace-service

---
+++ workspace-service/workspace-service/src/main/java/com/nexus/workspace_service/repository/WorkspaceRepository.java
@@ -0,0 +1,9 @@
+package com.nexus.workspace_service.repository;
+
+import com.nexus.workspace_service.entity.Workspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
+}
