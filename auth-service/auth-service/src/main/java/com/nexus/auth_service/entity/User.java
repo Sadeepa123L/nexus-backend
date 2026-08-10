e32dbec6b7bc262261bceca4cbcdbd47ef158b37
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-08-10 08:25:37 +0530

    feat:Create user entity class

---
+++ auth-service/auth-service/src/main/java/com/nexus/auth_service/entity/User.java
@@ -0,0 +1,24 @@
+package com.nexus.auth_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+    private String jobRole;
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
+}
