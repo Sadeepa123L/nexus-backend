2b45591ce400d7dc3ccb6b67373d84d655702e07
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-08-11 07:28:30 +0530

    feat:Add AuthResponseDTO class for user authentication response data transfer

---
+++ auth-service/auth-service/src/main/java/com/nexus/auth_service/dto/AuthResponseDTO.java
@@ -0,0 +1,14 @@
+package com.nexus.auth_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDTO {
+    private String token;
+    private String name;
+    private String jobRole;
+}
