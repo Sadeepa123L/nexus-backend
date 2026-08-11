eed23dd460d68e7768e16b3601f9f601ae6eeb99
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-08-11 07:27:38 +0530

    feat:Add UserRepository interface for user data access

---
+++ auth-service/auth-service/src/main/java/com/nexus/auth_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nexus.auth_service.repository;
+
+import com.nexus.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
